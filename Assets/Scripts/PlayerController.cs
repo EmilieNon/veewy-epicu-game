a8265feffa12052a535b864cf6977ba7b9d795de
BrianWTerry <89167021+BrianWTerry@users.noreply.github.com>
2022-09-27 22:49:02 -0700

    Basic player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Vector2 rawInput;
 
-    // Update is called once per frame
+    [SerializeField] float moveSpeed;
+
+
     void Update()
     {
-        
+        Move();
+        FlipSprite();
+    }
+
+    void FlipSprite()
+    {
+        if(rawInput.x != 0)
+        {
+            transform.localScale = new Vector2(MathF.Sign(rawInput.x), 1f);
+        }
+    }
+
+    void Move()
+    {
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+
+        newPos.x = transform.position.x + delta.x;
+        newPos.y = transform.position.y + delta.y;
+        transform.position = newPos;
+    }
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
     }
 }
